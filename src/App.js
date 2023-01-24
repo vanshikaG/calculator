e9731c6ab4cab9e9e73fb13218defd764b9f646a
vanshikaG <vanshikagautam22@gmail.com>
2023-01-24 13:36:11 +0530

    .

--- src/App.js
+++ src/App.js
@@ -23,3 +23,19 @@ function App() {
 }
 
 export default App;
+
+
+
+function display(val) {
+  document.getElementById("result").value += val;
+}
+
+function solve() {
+  let x = document.getElementById("result").value;
+  let y = eval(x);
+  document.getElementById("result").value = y;
+}
+
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
